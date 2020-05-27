5c1f3ccc25c28ba3b8f13954c1b8f1abb55efa83
Konstantin <kulik.konstantin@gmx.de>
2020-05-27 15:43:36 +0200

    Base-Code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController instance;
+    public GameObject gameOverText;
+    public Text scoreText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    private int score = 0;
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver == true)
+        {
+            if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    public void BirdScored()
+    {
+        if(gameOver == true)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
