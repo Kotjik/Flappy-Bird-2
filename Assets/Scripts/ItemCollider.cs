e42e2089a808aab74f474a18efa55f5168220aaf
Sabrina Hartl <30804260+96Sabiii@users.noreply.github.com>
2020-05-31 14:52:01 +0200

    random Item spawn

---
+++ Assets/Scripts/ItemCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ItemCollider : MonoBehaviour
+{
+    bool itemColl = false;
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Item Collision?
+        if (coll.name.StartsWith("ItemPrefab"))
+        {
+            // Get longer in next Move call
+            //itemColl = true;
+
+            // Remove the Food
+            Destroy(coll.gameObject);
+
+            //GameController.instance.BirdScored();
+            UnityEngine.Debug.Log("Bounds");
+        }
+        // Collided with Tail or Border
+        else
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+    // Item Prefab
+    public GameObject itemPrefab;
+    public float spawnTime = 4.0f;
+
+    private Vector2 screenBounds;
+
+    // Rect for Borders
+   //public RectTransform canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn items every 4 seconds, starting in 3
+        InvokeRepeating("Spawn", 3, spawnTime);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Spawn one item
+    void Spawn()
+    {
+        int y = (int)UnityEngine.Random.Range(-screenBounds.y, screenBounds.y);
+        int x = (int)screenBounds.x * 2;
+
+        // Instantiate the food at (x, y)
+       Instantiate(itemPrefab,
+                    new Vector2(x, y),
+                    Quaternion.identity); // default rotation */
+
+    }
+}
