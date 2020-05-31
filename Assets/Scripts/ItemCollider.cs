d751451dc9bba3c8d199a44ef2c66da92bf271fb
Sabrina Hartl <30804260+96Sabiii@users.noreply.github.com>
2020-05-31 17:27:15 +0200

    spawn and check different items

--- Assets/Scripts/ItemCollider.cs
+++ Assets/Scripts/ItemCollider.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 
 public class ItemCollider : MonoBehaviour
 {
+    //necessary?
     bool itemColl = false;
 
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        //Item Collision?
-        if (coll.name.StartsWith("ItemPrefab"))
+        //Item Collision
+        if (coll.name.StartsWith("ItemPrefab1"))
         {
             // Get longer in next Move call
             //itemColl = true;
 
-            // Remove the Food
+            // Remove the Item
             Destroy(coll.gameObject);
 
             //GameController.instance.BirdScored();
-            UnityEngine.Debug.Log("Bounds");
+            UnityEngine.Debug.Log("Bounds Item1");
         }
-        // Collided with Tail or Border
-        else
+        else if (coll.name.StartsWith("ItemPrefab2"))
         {
+            // Remove the Item
+            Destroy(coll.gameObject);
+
+            //GameController.instance.BirdScored();
+            UnityEngine.Debug.Log("Bounds Item2");
+        }
+        else if (coll.name.StartsWith("ItemPrefab3"))
+        {
+            // Remove the Item
+            Destroy(coll.gameObject);
+
+            //GameController.instance.BirdScored();
+            UnityEngine.Debug.Log("Bounds Item3");
         }
     }
 }
--- Assets/Scripts/SpawnItems.cs
+++ Assets/Scripts/SpawnItems.cs
@@ -7,7 +7,11 @@ using UnityEngine;
 public class SpawnItems : MonoBehaviour
 {
     // Item Prefab
-    public GameObject itemPrefab;
+    List<GameObject> prefabList = new List<GameObject>();
+    public GameObject itemPrefab1;
+    public GameObject itemPrefab2;
+    public GameObject itemPrefab3;
+
     public float spawnTime = 4.0f;
 
     private Vector2 screenBounds;
@@ -18,8 +22,13 @@ public class SpawnItems : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Spawn items every 4 seconds, starting in 3
-        InvokeRepeating("Spawn", 3, spawnTime);
+        //add possible Items to List
+        prefabList.Add(itemPrefab1);
+        prefabList.Add(itemPrefab2);
+        prefabList.Add(itemPrefab3);
+
+        // Spawn items every 4 seconds, starting in 2
+        InvokeRepeating("Spawn", 2, spawnTime);
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
@@ -28,9 +37,10 @@ public class SpawnItems : MonoBehaviour
     {
         int y = (int)UnityEngine.Random.Range(-screenBounds.y, screenBounds.y);
         int x = (int)screenBounds.x * 2;
-
+        int prefabIndex = UnityEngine.Random.Range(0, prefabList.Count);
+    
         // Instantiate the food at (x, y)
-       Instantiate(itemPrefab,
+        Instantiate(prefabList[prefabIndex],
                     new Vector2(x, y),
                     Quaternion.identity); // default rotation */
 
