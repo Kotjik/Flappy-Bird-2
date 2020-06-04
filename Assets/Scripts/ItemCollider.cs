4f1ab50ff235d361ebc7da00de08a151e5dc64d8
Sabrina Hartl <30804260+96Sabiii@users.noreply.github.com>
2020-06-04 15:38:39 +0200

    Add random item spawn

---
+++ Assets/Scripts/ItemCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ItemCollider : MonoBehaviour
+{
+    //necessary?
+    bool itemColl = false;
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Item Collision
+        if (coll.name.StartsWith("ItemPrefab1"))
+        {
+            // Get longer in next Move call
+            //itemColl = true;
+
+            // Remove the Item
+            Destroy(coll.gameObject);
+
+            //GameController.instance.BirdScored();
+            UnityEngine.Debug.Log("Bounds Item1");
+        }
+        else if (coll.name.StartsWith("ItemPrefab2"))
+        {
+            // Remove the Item
+            Destroy(coll.gameObject);
+
+            //GameController.instance.BirdScored();
+            UnityEngine.Debug.Log("Bounds Item2");
+        }
+        else if (coll.name.StartsWith("ItemPrefab3"))
+        {
+            // Remove the Item
+            Destroy(coll.gameObject);
+
+            //GameController.instance.BirdScored();
+            UnityEngine.Debug.Log("Bounds Item3");
+        }
+    }
+}
--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -6,18 +6,29 @@ public class ScrollingObject : MonoBehaviour
 {
 
     private Rigidbody2D rb2d;
+    private Vector2 screenBounds;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = new Vector2(GameController.instance.scrollSpeed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameController.instance.gameOver == true)
+        //delete unused prefabs
+        if (transform.position.x < -screenBounds.x)
+        {
+            if (transform.name.StartsWith("ItemPrefab"))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        if (GameController.instance.gameOver == true)
         {
             rb2d.velocity = Vector2.zero;
         }
