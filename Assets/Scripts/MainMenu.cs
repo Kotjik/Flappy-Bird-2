f2609e974b624e792b080e4ffe51703076e7f1d0
Vera Wittmann <31100655+veraarev@users.noreply.github.com>
2020-06-08 11:47:34 +0200

    adapt menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settingsPanel;
+
     // Start is called before the first frame update
     public void StartGame()
     {
@@ -15,6 +17,16 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         print("quit game");
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
     }
       
 }
