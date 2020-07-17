23d252349cf0a3bbf7c3a2f9506ce37ec54abf1f
AboutWhite <aboutwhiterabbit@gmail.com>
2020-07-17 16:03:11 +0200

    Added new Assets (Background, Dragon with Animations)

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    private SpriteRenderer r;
+    private float rHorizontalLength;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(speed, 0);
+
+        r = GetComponent<SpriteRenderer>();
+        rHorizontalLength = r.bounds.size.x;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity = new Vector2(speed, 0);
+
+        if (transform.position.x < -rHorizontalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(rHorizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
